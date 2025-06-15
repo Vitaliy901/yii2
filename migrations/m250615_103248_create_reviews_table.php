e7eed61e7e094acb2572725a9687f8f4b12509dc
vitaliy <allanamos90@gmail.com>
2025-06-15 14:07:20 +0300

    Added reviews migration table class.

---
+++ migrations/m250615_103248_create_reviews_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%reviews}}`.
+ */
+class m250615_103248_create_reviews_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%reviews}}', [
+            'id' => $this->primaryKey(),
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('{{%reviews}}');
+    }
+}
